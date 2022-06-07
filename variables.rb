7891d3799ba19ee7a5936205985b81d5af01ab62
Astrid Hecht <silentaviary@gmail.com>
2022-06-07 10:58:05 -0600

    Fixed integers being defined as strings

--- variables.rb
+++ variables.rb
@@ -1,6 +1,6 @@
 who_are_you = "Hi, I'm a string!"
 how_are_you = "Good, thanks!"
 whats_your_name = "Haha I don't know!"
-age = "900"
-number_toes = "13"
-bones_broken = "0"
+age = 900
+number_toes = 13
+bones_broken = 0
