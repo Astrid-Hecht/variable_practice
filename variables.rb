818912e572513ab3c282c6079d19a7d5a4c6d10f
Astrid Hecht <silentaviary@gmail.com>
2022-06-07 11:13:56 -0600

    Redifined variables within the code and made it have a conversation

--- variables.rb
+++ variables.rb
@@ -1,12 +1,26 @@
 who_are_you = "Hi, I'm a string!"
+puts who_are_you
 how_are_you = "Good, thanks!"
+question = ">How are you, string?"
+puts question
+puts how_are_you
 whats_your_name = "Haha I don't know!"
+question = ">What's your name?"
+puts question
+puts whats_your_name
+question = ">How old are you?"
 age = 900
+puts question
+puts age
 number_toes = 13
 bones_broken = 0
 bank_account = 0.03
 party_points = 420.69
 favorite_temp = 67.5
+puts ">Favorite temperature?"
+puts favorite_temp
 is_alive = true
 making_pancakes = true
 doesnt_know_variables = false
+whats_your_name = "Oh, actually, I'm Code!"
+puts whats_your_name
